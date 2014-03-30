33617f9475d82e36405798cd20aa894fee61dc75
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-03-30 11:39:44 +0530

    Android: 'SimpleCalc' Final

--- android/SimpleCalc/gen/com/ajaybhatia/simplecalc/R.java
+++ android/SimpleCalc/gen/com/ajaybhatia/simplecalc/R.java
@@ -24,7 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int button1=0x7f080002;
+        public static final int button2=0x7f080003;
+        public static final int editText1=0x7f080000;
+        public static final int editText2=0x7f080001;
+        public static final int textView1=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,8 +39,10 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int add_button=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int result_textview=0x7f050004;
+        public static final int subtract_button=0x7f050003;
     }
     public static final class style {
         /** 
--- android/SimpleCalc/src/com/ajaybhatia/simplecalc/MainActivity.java
+++ android/SimpleCalc/src/com/ajaybhatia/simplecalc/MainActivity.java
@@ -1,15 +1,26 @@
 package com.ajaybhatia.simplecalc;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button addButton = (Button) findViewById(R.id.button1);
+		Button subButton = (Button) findViewById(R.id.button2);
+		
+		addButton.setOnClickListener(this);
+		subButton.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +28,22 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		EditText et1 = (EditText) findViewById(R.id.editText1);
+		EditText et2 = (EditText) findViewById(R.id.editText2);
+		TextView tv = (TextView) findViewById(R.id.textView1);
+		
+		int num1 = Integer.parseInt(et1.getText().toString());
+		int num2 = Integer.parseInt(et2.getText().toString());
+		
+		if (v.getId() == R.id.button1) {
+			tv.setText(String.valueOf(num1 + num2));
+		} else {
+			tv.setText(String.valueOf(num1 - num2));
+		}
 	}
 
 }
