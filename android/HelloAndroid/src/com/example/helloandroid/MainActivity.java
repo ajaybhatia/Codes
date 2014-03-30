af0253f5735cfae2e9c33612a4a25d6293a451b0
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-03-30 11:01:00 +0530

    Android: "Hello Android" - Added a Button with Event Handler

--- android/HelloAndroid/src/com/example/helloandroid/MainActivity.java
+++ android/HelloAndroid/src/com/example/helloandroid/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.helloandroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button button1 = (Button) findViewById(R.id.button1);
+		button1.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +24,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		TextView tv = (TextView) findViewById(R.id.textView1);
+		tv.setText("Hi!, You have clicked \'Click Me\'");
 	}
 
 }
