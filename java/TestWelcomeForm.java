91c8196250fd688c314fbcee6850366ac2d8bb74
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-07-17 13:19:38 +0530

    Java: Change made to last commit by replacing AWT with swing and using FlowLayout Manager

--- java/TestWelcomeForm.java
+++ java/TestWelcomeForm.java
@@ -1,16 +1,8 @@
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 public class TestWelcomeForm {
 	public static void main(String[] args) {
 		WelcomeForm form = new WelcomeForm("My Form");
-		form.setSize(500, 500);
+		form.pack();
+		form.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
 		form.setVisible(true);
-
-		form.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				System.exit(1);
-			}
-		});
 	}	
 }
--- java/WelcomeForm.java
+++ java/WelcomeForm.java
@@ -1,7 +1,20 @@
-import java.awt.Frame;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
 
-public class WelcomeForm extends Frame {
+public class WelcomeForm extends JFrame {
+	private JButton[] button;
+
 	public WelcomeForm(String title) {
 		super(title);
+		
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		button = new JButton[50];
+		for(int i = 0; i < 50; i++) {
+			button[i] = new JButton("Button " + i); 
+			add(button[i]);
+		}
 	}
 }
