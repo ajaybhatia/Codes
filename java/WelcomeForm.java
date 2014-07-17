eae365caaa5c8b56f336de4a02d4c9eb330c612b
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-07-17 13:23:20 +0530

    Java: Changes made to WelcomeForm.java to use BorderLayout instead FlowLayout

--- java/WelcomeForm.java
+++ java/WelcomeForm.java
@@ -1,7 +1,7 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 
 public class WelcomeForm extends JFrame {
 	private JButton[] button;
@@ -9,12 +9,19 @@ public class WelcomeForm extends JFrame {
 	public WelcomeForm(String title) {
 		super(title);
 		
-		setLayout(new FlowLayout(FlowLayout.LEFT));
+		setLayout(new BorderLayout());
 
-		button = new JButton[50];
-		for(int i = 0; i < 50; i++) {
-			button[i] = new JButton("Button " + i); 
-			add(button[i]);
-		}
+		button = new JButton[5];
+		button[0] = new JButton("North Button");
+		button[1] = new JButton("East Button");
+		button[2] = new JButton("West Button");
+		button[3] = new JButton("South Button");
+		button[4] = new JButton("Center Button");
+
+		add(button[0], BorderLayout.NORTH);
+		add(button[1], "East");
+		add(button[2], "West");
+		add(button[3], "South");
+		add(button[4], "Center");
 	}
 }
