28b0e7f4d314f460e1a10dc90c52fa9eda24a775
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-08-01 23:11:12 +0530

    java: Use of Math class

---
+++ java/UsingMaths.java
@@ -0,0 +1,33 @@
+/*
+ * UsingMaths.java
+ * 
+ * Copyright 2014 Ajay Bhatia <ajay@dumb-box>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class UsingMaths {
+	public static void main (String args[]) {
+		double x = Double.parseDouble(args[0]);
+		double y = Double.parseDouble(args[1]);
+		
+		System.out.printf("%.2f raise to power %.2f is %.2f\n", x, y, Math.pow(x, y));
+	}
+}
+
