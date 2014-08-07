ef45d1a0c0c69baf3ba894b9316fb18e1a9a075d
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-08-07 09:41:30 +0530

    Java: Use of While loop

---
+++ java/WhileLoop.java
@@ -0,0 +1,13 @@
+// Application of the while loop for printing 1 to 100 integers on the screen
+
+public class WhileLoop {
+	public static void main(String[] args) {
+		int i = 1;	
+	
+		while (i <= 100) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+}
