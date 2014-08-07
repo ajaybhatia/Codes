c80aff782f5ba30ce24304f8c4818e1e308de245
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-08-07 09:41:48 +0530

    Java: Use of do-while loop

---
+++ java/DoWhileLoop.java
@@ -0,0 +1,11 @@
+// Application of the do while loop for printing 1 to 100 integers on the screen
+
+public class DoWhileLoop {
+	public static void main(String[] args) {
+		int i = 1; 
+		
+		do 
+			System.out.println(i); 
+		while (i++ <= 100);
+	}
+}
