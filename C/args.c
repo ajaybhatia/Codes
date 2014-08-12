e87e30e3db8bd0713c3d30984f067b7e62f6a58e
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-08-12 14:00:07 +0530

    C: Made changes to last commit (command-line argument code)

--- C/args.c
+++ C/args.c
@@ -26,8 +26,7 @@
 	
 int main(int argc, char **argv)
 {
-	printf("%d\n", argc);
-	printf("%s\n", argv[1]);
+	printf("%d + %d = %d\n", atoi(argv[1]), atoi(argv[2]), (atoi(argv[1]) + atoi(argv[2])));
 	return 0;
 }
 
