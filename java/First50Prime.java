03ac02544fe02b7969054c833aefb42b7fee7a11
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-08-21 23:46:49 +0530

    Java: Program to print first 50 prime numbers

---
+++ java/First50Prime.java
@@ -0,0 +1,46 @@
+/*
+ * First50Prime.java
+ * 
+ * Copyright 2014 Ajay Bhatia <ajay@dumb-box>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class First50Prime {
+	public static void main (String args[]) {
+		int count = 0;
+		for (int i = 2;; i++) {
+			boolean isPrime = true;	
+			for (int j = 2; j <= Math.sqrt(i); j++) {
+				if (i % j == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			
+			if (isPrime)
+				if (count++ != 50)
+					System.out.printf("%4d\t", i);
+				else
+					break;
+		}
+		System.out.println();
+	}
+}
+
