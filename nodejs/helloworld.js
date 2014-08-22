6fde8d46ea2086957af3835b1fc4715b94ec9adc
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-08-22 11:10:15 +0530

    nodejs: Made changes to helloworld.js to open other pages in the path

--- nodejs/helloworld.js
+++ nodejs/helloworld.js
@@ -1,8 +1,25 @@
 var http = require("http");
 
 http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello, World!');
+	// Normalize URL by removing querystring, optional
+	// trailing slash and make it lowercase
+	var path = req.url.path(/\/?(?:\?.*)?$/, '').toLowerCase();
+
+	swtich (path) {
+		case '':
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.end('Homepage');
+			break;
+
+		case '/about':
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.end('About');
+			break;
+
+		default:
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.end('Not Found');
+	}
 }).listen(3000);
 
 console.log('Server started on localhost:3000, press Ctrl-C to terminate....');
