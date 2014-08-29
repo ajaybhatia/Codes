b89c9280c25608dbd407678366c79e27d35b3a86
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-08-29 09:13:39 +0530

    Java: Simple method to print. The method mimics println()

---
+++ java/PrintMethod.java
@@ -0,0 +1,38 @@
+/*
+ * PrintMethod.java
+ * 
+ * Copyright 2014 Ajay Bhatia <ajay@dumb-box>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class PrintMethod {
+
+	static void print(Object obj) {
+		System.out.println(obj);
+	}	 
+
+	public static void main (String args[]) {
+		print("Hello, World");
+		print(1000);
+		print(Math.PI);
+		print('a');
+	}
+}
+
