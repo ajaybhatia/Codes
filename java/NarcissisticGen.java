3de84b980537ae08ec22e03a8d09fa2962627c83
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-09-03 11:35:50 +0530

    Java: Removed unnecessary custom pow() from NarcissisticGen.java

--- java/NarcissisticGen.java
+++ java/NarcissisticGen.java
@@ -1,11 +1,4 @@
 public class NarcissisticGen {
-	public static int pow(int x, int y) {
-		if (y == 0)
-			return 1;
-		else
-			return x * pow(x, y - 1);	
-	}
-	
 	public static boolean isNarcissistic(long n) {
 		long temp = n;
 		long num = 0;
@@ -15,13 +8,13 @@ public class NarcissisticGen {
 			long digit = n % 10;
 			n /= 10;
 			num += Math.pow((int)digit, length);	
-		}	
+		}
 			
 		return (num == temp) ? true : false;
 	}
 	
 	public static void main(String[] args) {
-		for (int i = 100; i <= 100000000; i++)
+		for (int i = 100; i <= 10000; i++)
 			if (isNarcissistic(i))
 				System.out.println(i);
 	}
