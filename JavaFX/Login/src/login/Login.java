b1f6d52718eb023ecc11f66a08b84c955d00bb98
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-10-04 13:36:07 +0530

    JavaFX: Added GridPane and scene to Main Application

--- JavaFX/Login/src/login/Login.java
+++ JavaFX/Login/src/login/Login.java
@@ -26,8 +26,11 @@ package login;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -40,6 +43,16 @@ public class Login extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("JavaFX Welcome");
+        
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        
+        Scene scene = new Scene(grid, 300, 275);
+        
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
