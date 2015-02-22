328c827bed3fb0df03f52ed2288695babfbf2098
Ajay Bhatia <prof.ajaybhatia@gmail.com>
2015-02-22 14:39:38 +0530

    cpp(Matrix): Initial commit

---
+++ cpp/Matrix/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: ajay
+ *
+ * Created on 22 February, 2015, 2:36 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
